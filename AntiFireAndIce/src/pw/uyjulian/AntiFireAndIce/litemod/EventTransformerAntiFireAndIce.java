a3e28b1d288c576237b5d67a03ac187bbcfddf39
uyjulian <uyjulian@gmail.com>
2015-11-14 21:03:41 -0600

    Initial fixes

--- AntiFireAndIce/src/pw/uyjulian/AntiFireAndIce/litemod/EventTransformerAntiFireAndIce.java
+++ AntiFireAndIce/src/pw/uyjulian/AntiFireAndIce/litemod/EventTransformerAntiFireAndIce.java
@@ -11,8 +11,8 @@ public class EventTransformerAntiFireAndIce extends EventInjectionTransformer {
 
 	@Override
 	protected void addEvents() {
-		this.addEvent(Event.getOrCreate("FireAndIce_BlockFire_init", true), new MethodInfo(ObfTableAntiFireAndIce.BlockFire, ObfTableAntiFireAndIce.BlockFire_init, Void.TYPE), new MethodHead()).addListener(new MethodInfo("pw.uyjulian.FireAndIce.litemod.LiteModAntiFireAndIce", "BlockFireInit"));
-		this.addEvent(Event.getOrCreate("FireAndIce_BlockIce_UpdateTick", true), new MethodInfo(ObfTableAntiFireAndIce.BlockIce, ObfTableAntiFireAndIce.BlockIce_updateTick, Void.TYPE, ObfTableAntiFireAndIce.World, ObfTableAntiFireAndIce.BlockPos, ObfTableAntiFireAndIce.IBlockState, "java/util/Random"), new MethodHead()).addListener(new MethodInfo("pw.uyjulian.FireAndIce.litemod.LiteModAntiFireAndIce", "BlockFireInit"));
+		this.addEvent(Event.getOrCreate("FireAndIce_BlockFire_init", true), new MethodInfo(ObfTableAntiFireAndIce.BlockFire, ObfTableAntiFireAndIce.BlockFire_init, Void.TYPE), new MethodHead()).addListener(new MethodInfo("pw.uyjulian.AntiFireAndIce.litemod.LiteModAntiFireAndIce", "BlockFireInit"));
+		this.addEvent(Event.getOrCreate("FireAndIce_BlockIce_UpdateTick", true), new MethodInfo(ObfTableAntiFireAndIce.BlockIce, ObfTableAntiFireAndIce.BlockIce_updateTick, Void.TYPE, ObfTableAntiFireAndIce.World, ObfTableAntiFireAndIce.BlockPos, ObfTableAntiFireAndIce.IBlockState, "java/util/Random"), new MethodHead()).addListener(new MethodInfo("pw.uyjulian.AntiFireAndIce.litemod.LiteModAntiFireAndIce", "BlockFireInit"));
 		
 	}
 
